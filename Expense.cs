43e3279d55337481f0c81b05ec1ad60a292d7867
ssofiyas <sos00065@laurea.fi>
2026-03-24 19:08:15 +0200

    Added Expense model and database initialization

---
+++ Expense.cs
@@ -0,0 +1,10 @@
+namespace PersonalExpenseTracker;
+
+public class Expense
+{
+    public int Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using Microsoft.Data.Sqlite;
+using Dapper;
+using PersonalExpenseTracker;
+
+// tietokanta polku
+string connectionString = "Data Source=expenses.db";
+
+Console.WriteLine("=== Personal Expense Tracker - Alustus ===");
+
+// yhteys tietokantaan ja taulun luominen
+using (var connection = new SqliteConnection(connectionString))
+{
+    // SQL 
+    string createTableSql = @"
+        CREATE TABLE IF NOT EXISTS Expenses (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Description TEXT NOT NULL,
+            Amount DECIMAL NOT NULL,
+            Category TEXT NOT NULL,
+            Date TEXT NOT NULL
+        );";
+
+    connection.Execute(createTableSql);
+    Console.WriteLine("[OK] Database and table initialized.");
+}
+
+Console.WriteLine("Day 1: Basic structure initialized!");
